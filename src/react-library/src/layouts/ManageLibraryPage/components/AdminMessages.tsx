93ba4da7096eeaaff162f365873dfef7bf77b1d3
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2025-10-17 16:40:42 +0200

    I've updated the code to use optional chaining (?.) instead of the logical AND (&&) operator.

--- src/react-library/src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/react-library/src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -27,7 +27,7 @@ export const AdminMessages = () => {
 
     useEffect(() => {
         const fetchUserMessages = async () => {
-            if (authState && authState.isAuthenticated) {
+            if (authState?.isAuthenticated) {
                 const url = `http://localhost:8080/api/messages/search/findByClosed/?closed=false&page=${currentPage - 1}&size=${messagesPerPage}`;
                 const requestOptions = {
                     method: 'GET',
